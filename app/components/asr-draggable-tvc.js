7ea909fffaea40d9aa82dc128c95ed0cb5c9b51d
Andres Serrano <andres.srg@gmail.com>
2014-06-30 18:54:24 -0500

    implemented hotizontal and vertical locking when dragging start

--- app/components/asr-draggable-tvc.js
+++ app/components/asr-draggable-tvc.js
@@ -7,12 +7,16 @@ export default Ember.Component.extend({
   cellOffset: 0,
   baseWidth: null,
 
+  // initial drag x or y
+  isDraggingInY: false,
+  isDraggingInX: false,
+
   initializeWrapperWidth: function() {
     var initialWidth = this.$().width();
 
     this.set('baseWidth', initialWidth);
 
-    this.$('.asr-draggable-tvc-wrapper').width(initialWidth * 3);
+    this.$().width(initialWidth * 3);
 
     this.$('asr-draggable-tvc-left').width(initialWidth);
     this.$('asr-draggable-tvc-main').width(initialWidth);
@@ -28,31 +32,49 @@ export default Ember.Component.extend({
     var baseWidth = this.get('baseWidth');
     var cellOffset = this.get('cellOffset');
     var totalOffset = cellOffset - baseWidth;
-    this.$('.asr-draggable-tvc-wrapper').css('transform', 'translateX(' + totalOffset + 'px)');
+    this.$().css('transform', 'translateX(' + totalOffset + 'px)');
   }.observes('cellOffset', 'baseWidth'),
 
   gestures: {
     drag: function(event) {
       var deltaX = event.gesture.deltaX;
 
-      // event.gesture.preventDefault();
-      if (Math.abs(event.gesture.deltaX) < Math.abs(event.gesture.deltaY)) {
-        // event.gesture.preventDefault();
+      if (!this.get('isDraggingInX') && !this.get('isDraggingInY')) {
+        if (Math.abs(event.gesture.deltaX) < Math.abs(event.gesture.deltaY)) {
+          // began dragging vertically
+          this.set('isDraggingInY', true);
+          this.set('isDraggingInX', false);
+          return false;
+        } else {
+          // began dragging horizontally
+          this.set('isDraggingInY', false);
+          this.set('isDraggingInX', true);
+        }
+      }
+
+      if (this.get('isDraggingInY')) {
         return false;
       }
+
+      // is dragging horizontally
       event.gesture.preventDefault();
 
       if (Math.abs(deltaX) > this.get('baseWidth') * 0.5) {
         this.$('asr-draggable-tvc-left').css('background', 'yellow');
       }
 
-      this.$('.asr-draggable-tvc-wrapper').removeClass('animate');
+      this.$().removeClass('animate');
       this.set('cellOffset', deltaX);
       return false;
     },
     dragend: function() {
-      this.$('.asr-draggable-tvc-wrapper').addClass('animate');
+      this.$().addClass('animate');
       this.set('cellOffset', 0);
+
+      // reset initial drag x or y
+      this.set('isDraggingInY', false);
+      this.set('isDraggingInX', false);
+
       return false;
     },
 
